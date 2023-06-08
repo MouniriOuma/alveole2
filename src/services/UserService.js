511e7771a0ed8efa8fb8488a2a69469093bacff6
MouniriOuma <mouniri.ouma@gmail.com>
2023-06-08 09:26:41 +0100

    adding some api URLs

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,32 +1,34 @@
 import axios from 'axios';
 
 const USER_API_BASE_URL = "http://localhost:8081/users";
-const CREATE_USER_API_BASE_URL = "http://localhost:8081/api/auth";
+const SIGN_USER_API_BASE_URL = "http://localhost:8081/api/auth";
 
 class UserService {
   getUsers() {
     return axios.get(USER_API_BASE_URL);
   }
 
-  createUser(user) {
-    return axios.post(USER_API_BASE_URL, user);
-  }
-
-  registerUser(user) {
-    return axios.post(CREATE_USER_API_BASE_URL + '/signup', user);
-  }
-
   getUserById(userId) {
     return axios.get(USER_API_BASE_URL + '/' + userId);
-  }
-
-  updateUser(userId, user) {
-    return axios.put(CREATE_USER_API_BASE_URL + '/users/' + userId, user);
   }
 
   deleteUser(userId) {
     return axios.delete(USER_API_BASE_URL + '/' + userId);
   }
+
+  registerUser(user) {
+    return axios.post(SIGN_USER_API_BASE_URL + '/signup', user);
+  }
+
+  updateUser(userId, user) {
+    return axios.put(SIGN_USER_API_BASE_URL + '/users/' + userId, user);
+  }
+
+  getUserRoleByUsername(username) {
+    return axios.get(USER_API_BASE_URL + '/role/' + username);
+  }
+
+
 }
 
 export default new UserService();
